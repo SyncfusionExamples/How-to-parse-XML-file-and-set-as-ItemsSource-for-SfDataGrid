16dcc73cd7cb3e66944c7602c9717915d561b3ef
SethupathyDevarajan <sethupathy.sf3898@gmail.com>
2023-12-21 11:26:00 +0530

    Code Updated.

---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.Maui.Data;
+using Syncfusion.Maui.DataGrid;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream? stream = assembly.GetManifestResourceStream("SfDataGridSample.Repository.xml");
+            ObservableCollection<OrderInfo> OrdersInfo;
+            XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<OrderInfo>));
+            OrdersInfo = (ObservableCollection<OrderInfo>)serializer.Deserialize(stream!)!;
+            dataGrid.ItemsSource = OrdersInfo;
+
+        }
+    }
+}
---
+++ SfDataGridSample/Model/OrderInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGridSample
+{
+    public class OrderInfo
+    {
+        public int OrderID { get; set; }
+        public int EmployeeID { get; set; }
+        public string CustomerID { get; set; }
+        public string ShipCountry { get; set; }     
+
+        public OrderInfo()
+        {
+
+        }
+    }
+}
